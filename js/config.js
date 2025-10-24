12a57c987b7e6cf4f3005de5b24200dd9ac2d283
JhonPizaVision <jhon.piza@visionymarketing.com.co>
2025-10-24 16:29:33 -0500

    Change GitHub token in config.js
    Updated GitHub token in config.js.

--- js/config.js
+++ js/config.js
@@ -5,7 +5,7 @@ const CONFIG = {
     baseURL: `https://api.github.com/repos/JhonPizaVision/VisionyMarketing/git/trees/main?recursive=1`,
     rutaBase: "DocumentosClientes",
     // Token de GitHub - Reemplaza con tu token real
-    token: "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"
+    token: "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"
 };
 
 // Obtener ruta actual desde URL
